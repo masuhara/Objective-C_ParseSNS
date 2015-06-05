6fb2aed457de55e2bf9e0fd6e91e2463b1f98db6
Daisuke Masuhara <info@masuhara.net>
2015-06-05 18:50:53 +0900

    Initial Commit

---
+++ ParseSNS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ParseSNS
+//
+//  Created by Master on 2015/06/05.
+//  Copyright (c) 2015年 net.masuhara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ParseSNS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ParseSNS
+//
+//  Created by Master on 2015/06/05.
+//  Copyright (c) 2015年 net.masuhara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
